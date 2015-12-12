ab3098c7bf86753e36255b65aaeeb348d7e512d9
guohuiGH <guohui2011915@gmail.com>
2015-12-12 07:14:13 +0000

    clear data

---
+++ script/join_daily.py
@@ -0,0 +1,33 @@
+#########################################################################
+# File Name: connect_info.py
+# Author: 
+# mail: 
+# Created Time: Wed 25 Nov 2015 08:21:04 AM UTC
+#########################################################################
+#!/bin/bash
+#coding=utf-8
+
+import sys
+
+def connect_file():
+    if sys.argv[3] == '1':
+        myfile = open('../data/test/avazu_user_quantify', 'w+')
+        #myfile = open('../data/test/user_info_' + str(sys.argv[1]) + '_' + str(sys.argv[2]), 'w')
+    else:
+        myfile = open('../data/train/avazu_user_quantify', 'w+')
+        #myfile = open('../data/train/user_info_' + str(sys.argv[1]) + '_' + str(sys.argv[2]), 'w')
+    for i in range(int(sys.argv[1]), int(sys.argv[2]) + 1):
+        try:
+            myfile2 = open('../data/quantify/avazu_user_quantify_' + str(i))
+        except:
+            #print i
+            continue
+        line = myfile2.readline()
+        while line:
+            myfile.write(line)
+            line = myfile2.readline()
+        myfile2.close()
+    myfile.close()
+
+if __name__=='__main__':
+    connect_file()
--- script/quantify_test_user.py
+++ script/quantify_test_user.py
@@ -9,78 +9,80 @@
 import sys
 
 def read_ad_file():
-    map_ad_file = open('../tmp/avazu_map_ad')
-    line = map_ad_file.readline()
-    map_ad = dict()
-    while line:
-        line_list = line.strip().split(',')
-        map_ad[line_list[0]] = line_list[1]
-        line = map_ad_file.readline()
-    map_ad_file.close()
+    #map_ad_file = open('../data/ad/avazu_map_ad')
+    #line = map_ad_file.readline()
+    #map_ad = dict()
+    #while line:
+    #    line_list = line.strip().split(',')
+    #    map_ad[line_list[1]] = line_list[0]
+    #    line = map_ad_file.readline()
+    #map_ad_file.close()
     return map_ad
+    
 
-def read_user_file():
-    map_user_file = open('../tmp/avazu_map_user')
-    line = map_user_file.readline()
-    map_user = dict()
-    reverse_map_user = dict()
-    while line:
-        line_list = line.strip().split(',')
-        map_user[line_list[0]] = line_list[1]
-        reverse_map_user[line_list[1]] = line_list[0]
-        line = map_user_file.readline()
-    map_user_file.close()
 
-    sample_user_file = open('../tmp/avazu_sample_user_after_delete')
-    sample_user_list = [line.strip().split(',')[1] for line in sample_user_file.readlines()]
+
+def read_file():
+    #map_user_file = open('../data/user/avazu_map_user')
+    #line = map_user_file.readline()
+    #map_user = dict()
+    #reverse_map_user = dict()
+    #while line:
+    #    line_list = line.strip().split(',')
+    #    map_user[line_list[0]] = line_list[1]
+    #    reverse_map_user[line_list[1]] = line_list[0]
+    #    line = map_user_file.readline()
+    #map_user_file.close()
+
+    sample_user_file = open('../data/train/avazu_sample_user_after_delete')
+    lines = sample_user_file.readlines()
+    sample_user_list = [line.strip().split(',')[1] for line in lines]
+    ad_list = [line.strip().split(',')[0] for line in lines]
     sample_user_file.close()
-
+    
+    #sample_user = dict()
     sample_user = dict()
     for user in sample_user_list:
-        sample_user[reverse_map_user[user]] = user
-    print len(sample_user)
-    return sample_user
+        #sample_user[reverse_map_user[user]] = user
+        sample_user[user] = 1
+    sample_ad = dict()
+    for ad in ad_list:
+        sample_ad[ad] = 1
+    print len(sample_user), len(sample_ad)
+    return sample_user, sample_ad
 
 def read_test_user(map_ad, map_sample_user):
-    test_user_file = open('../data/user/avazu_user_info_' + argv[1])
-    test_user_quantify_file = open('../tmp/avazu_user_test_quantify', 'w+')
-    user_key = map_sample_user.keys()
-    ad_key = map_ad.keys()
+    test_user_file = open('../data/test/avazu_user_quantify')
+    test_user_quantify_file = open('../data/test/avazu_sample_user2', 'w+')
+    #user_key = map_sample_user.keys()
+    #ad_key = map_ad.keys()
     line = test_user_file.readline()
     count = 0; count_invalid_ad = 0; count_invalid = 0
+    count_invalid_user = dict()
     while line:
-        temp_line = ''
-        count += 1
-        line_list = line.strip().split('@@')
 
-        if line_list[1] not in user_key:
+        count += 1
+        line_list = line.strip().split(',')
+
+        if line_list[1] not in map_sample_user:
             line = test_user_file.readline()
             count_invalid += 1
             continue
-        if line_list[0] not in ad_key:
-            line = test_user_file.readline()
+        count_invalid_user[line_list[1]] = 1
+        if line_list[0] not in map_ad:
+       #     line = test_user_file.readline()
             count_invalid_ad +=1
-            continue
-        temp_line += map_ad[line_list[0]] + ','
-        temp_line += map_sample_user[line_list[1]] + ','
+            #continue
 
-        try:
-            click_value = line_list[2]
-        except:
-            click_value = '2105'
-            print line
-        if click_value < '2104':
-            temp_line += '1'
-        else:
-            temp_line += '0'
-        temp_line += '\n'
-        test_user_quantify_file.write(temp_line)
+        
+        test_user_quantify_file.write(line)
         line = test_user_file.readline()
     test_user_file.close()
     test_user_quantify_file.close()
-    print count, count_invalid_ad,count_invalid
+    print count, count_invalid_ad,count_invalid, len(count_invalid_user)
         
 if __name__=='__main__':
-    map_ad = read_ad_file()
-    map_sample_user = read_user_file()
+
+    (map_sample_user,map_ad) = read_file()
+
     read_test_user(map_ad, map_sample_user)
